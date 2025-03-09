a9006430cbe86602e21f9d8b6070374af8f6cf25
hemanialaparthi <alaparthi01@allegheny.edu>
2025-03-08 23:10:52 -0500

    Initial Commit

---
+++ NoDrunkTextTests/NoDrunkTextTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoDrunkTextTests.swift
+//  NoDrunkTextTests
+//
+//  Created by Hemani Alaparthi on 3/8/25.
+//
+
+import Testing
+@testable import NoDrunkText
+
+struct NoDrunkTextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
